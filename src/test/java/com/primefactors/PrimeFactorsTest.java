ebd5391c67212864e5bcf819fdf40523a101ef86
Luis Carlin <luiscarlin@gmail.com>
2017-02-27 05:31:52 -0500

    prime factors of 1 is empty

--- src/test/java/com/primefactors/PrimeFactorsTest.java
+++ src/test/java/com/primefactors/PrimeFactorsTest.java
@@ -1,4 +1,16 @@
 package com.primefactors;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class PrimeFactorsTest {
+    @Test
+    public void primeFactorOfOneIsEmpty() {
+        PrimeFactors primeFactor = new PrimeFactors();
+        assertThat(primeFactor.calculate(1), is(Arrays.asList()));
+    }
 }
