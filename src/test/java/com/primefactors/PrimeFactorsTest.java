6f8eac644f9b1a08fcdde18dd2e972537342b582
Luis Carlin <luiscarlin@gmail.com>
2017-02-27 05:38:52 -0500

    prime factors of 2 is 2

--- src/test/java/com/primefactors/PrimeFactorsTest.java
+++ src/test/java/com/primefactors/PrimeFactorsTest.java
@@ -1,5 +1,6 @@
 package com.primefactors;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -8,9 +9,21 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PrimeFactorsTest {
+
+    PrimeFactors underTest;
+
+    @Before
+    public void setUp() throws Exception {
+        underTest = new PrimeFactors();
+    }
+
     @Test
     public void primeFactorOfOneIsEmpty() {
-        PrimeFactors primeFactor = new PrimeFactors();
-        assertThat(primeFactor.calculate(1), is(Arrays.asList()));
+        assertThat(underTest.calculate(1), is(Arrays.asList()));
+    }
+
+    @Test
+    public void primeFactorsOfTwoIsTwo() throws Exception {
+        assertThat(underTest.calculate(2), is(Arrays.asList(2)));
     }
 }
